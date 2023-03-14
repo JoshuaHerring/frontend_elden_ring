bda6f83e3e749ae09d0b27ac30a01d66df1d6a7a
JoshuaHerring <jdherring2222@gmail.com>
2023-03-14 14:51:11 -0600

    ADDED SOME COMMENTS TO js so michael dosent die

--- js/index.js
+++ js/index.js
@@ -1,20 +1,39 @@
+/**
+* Fetchs the data from my backend api and returns it
+*/
 async function getBosses() {
     const response = await fetch("https://project-2-zf2s.onrender.com/bosses");
     const data = await response.json();
     return data;
 }
 
+/**
+ * I'm lazy and though all the rest of my code in this one big function
+ */
 async function renderBosses(){
+    //stores api data in bosses
     const bosses = await getBosses();
+
+    // You can inspect the browser and view the console tab. There the data is printed out to help you visualize it
+    console.log(bosses);
+
+    // gets my html div so I can interact with it and add stuff to it
     let holder = document.getElementById("holder")
+
+    //loops through the boss data wihch is sotred as a large array (look to the console to see all the data logged there)
     bosses.forEach(boss => {
 
+        //creates a ul in order to hold all of the traits to a specific boss and adds a class to it
         const bossHolder = document.createElement("ul");
         bossHolder.classList.add("BossHolder");
+
+        //loops through the different traits of the boss
         for (let key in boss) {
 
+            // if the trait is the id number do nothing
             if (key == "_id"){null}
 
+            // if the key is the name make it a header add the boss name to the html and give it a class then add it to the ul holder
             else if (key == "Name"){
                 let bossTrait = document.createElement("h2");
                 bossTrait.innerHTML = boss[key];
@@ -23,6 +42,7 @@ async function renderBosses(){
                 bossHolder.append(bossTrait);
             }
 
+            // if the key has a list inside of it do the same thing as the name value but also iterate though the list inside and add all of the list items as individual list items
             else if (typeof(boss[key]) == "object")
             {
                 let bossTrait = document.createElement("ul");
@@ -43,6 +63,7 @@ async function renderBosses(){
                 bossHolder.append(bossTrait);
             }
 
+            // if the key is anyting else (all that's left is key value pairs) print them out and give them a class name according to thier key
             else
             {
                 let bossTrait = document.createElement("li");
@@ -52,9 +73,12 @@ async function renderBosses(){
                 bossHolder.append(bossTrait);
             }
         }
+        // add the boss we just created to the html file using the div element in the index.html file that we stored in a variable at the begining
         holder.append(bossHolder);
+        // add a horiontal line after each boss
         bossHolder.append(document.createElement("hr"));
     });
 }
 
+//calls my do everything function
 renderBosses();
